05971282226e584ac8771e63949422db58d84309
ashraf akl <abouhajerakl@gmail.com>
2014-06-05 19:53:22 +0200

    Remove unwanted settings media path in inFocus

--- modules/infocus/settings.php
+++ modules/infocus/settings.php
@@ -42,11 +42,6 @@ return array(
                 'path' => 'multimedia/infocus',
                 'info' => array('crob' => false, 'width' => 396, 'height' => 232, 'exact' => false, 'allowedUploadRatio' => 8),
             ),
-            'slider' => array(
-                'autoSave' => true,
-                'path' => 'multimedia/infocus/slider',
-                'info' => array('crob' => true, 'width' => 260, 'height' => 155, 'exact' => true, 'allowedUploadRatio' => 8),
-            ),
             'list' => array(
                 'autoSave' => true,
                 'path' => 'multimedia/infocus/list',
