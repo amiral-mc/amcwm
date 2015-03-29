fd9053470dcf44eb4a354ff545149497e19da094
Ashraf Akl <ashrafakl@yahoo.com>
2015-03-29 12:43:38 +0200

    Add products xml sitemap generator class

--- commands/AmcXmlSitemapCommand.php
+++ commands/AmcXmlSitemapCommand.php
@@ -4,17 +4,18 @@ AmcWm::import("amcwm.commands.components.xmlSitemap.*");
 
 class AmcXmlSitemapCommand extends CConsoleCommand {
 
-    public function actionIndex($module = 'news', $sub = null, $route = null, $lang = null) {
-        $xmlClassData = "Amc" . ucfirst($module) . ucfirst($sub) . "XmlSitemapData";
+    public function actionIndex($module = 'news', $sub = null, $route = null, $lang = null) {        
+        $xmlClassData = "Amc" . ucfirst($module) . ucfirst($sub) . "XmlSitemapData";        
         $id = $module;
         if ($sub) {
             $id .= "/{$sub}";
         }
-        $xmlClassObject = new $xmlClassData($this, $id, $lang);
+        $xmlClassObject = new $xmlClassData($this, $id, $lang);        
         $xmlClassObject->setRoute($route);
         $ok = $xmlClassObject->generate();
-        if ($ok)
-            echo "XML Site Map has been generated";
+        if ($ok){
+            echo "XML Site Map has been generated\n";
+        }        
     }
 
     public function init() {
--- commands/components/xmlSitemap/AmcProductsXmlSitemapData.php
+++ commands/components/xmlSitemap/AmcProductsXmlSitemapData.php
@@ -13,19 +13,14 @@
  * @author Amiral Management Corporation
  * @version 1.0
  */
-class AmcArticlesXmlSitemapData extends AmcXmlSitemapData {
+class AmcProductsXmlSitemapData extends AmcXmlSitemapData {
 
     /**
      *
      * @var string current route 
      */
-    protected $route = 'articles/default/view';
-
-    /**
-     *
-     * @var string table to get data from 
-     */
-    protected $table = "articles";
+    protected $route = 'products/default/view';
+   
 
     /**
      *
@@ -33,7 +28,7 @@ class AmcArticlesXmlSitemapData extends AmcXmlSitemapData {
      * @return boolean
      */
     public function createXmlLinks() {
-        $list = new ArticlesListData(array($this->table), 0, $this->limit);
+        $list = new ProductsListData(array(), 0, $this->limit);
         $dateTime = time();
         $list->setFromDate(date("Y-m-d H:00:00", $dateTime - $this->period));
         $list->setToDate(date("Y-m-d H:59:59", $dateTime - $this->period));
@@ -43,18 +38,11 @@ class AmcArticlesXmlSitemapData extends AmcXmlSitemapData {
         $list->setAutoGenerate(false);
         $list->addColumn("create_date");
         $list->addColumn("publish_date");        
-        if ($this->table == 'articles') {
-            $articlesTables = ArticlesListData::getArticlesTables();
-            foreach ($articlesTables as $articleTable) {
-                $list->addJoin("LEFT JOIN {$articleTable} ON t.article_id = {$articleTable}.article_id");
-                $list->addWhere("{$articleTable}.article_id IS NULL");
-            }
-        }
         $list->generate();
-        $articles = $list->getQuery()->queryAll();
+        $products = $list->getQuery()->queryAll();
         $fileName = $this->generateXmlFileName($dateTime);
-        if (count($articles)) {
-            $ok = $this->generateMap($fileName, $articles, 'article_id', 'article_header');
+        if (count($products)) {
+            $ok = $this->generateMap($fileName, $products, 'product_id', 'product_name');
             return $ok;
         }
     }
