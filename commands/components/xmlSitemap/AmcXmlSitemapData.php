24764f1eb3af37cabc07dd354ba41b506ed1b0ba
Ashraf Akl <ashrafakl@yahoo.com>
2015-10-19 11:16:25 +0200

    Change xml sitemap data default period value

--- commands/components/xmlSitemap/AmcXmlSitemapData.php
+++ commands/components/xmlSitemap/AmcXmlSitemapData.php
@@ -42,7 +42,7 @@ abstract class AmcXmlSitemapData extends CComponent {
     /**    
      * @var integer Period time in seconds, 
      */
-    protected $period = 3600;       
+    protected $period = 86400;       
     
     /**
      *
