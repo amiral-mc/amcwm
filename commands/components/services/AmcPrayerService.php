4be168360060c51f7d577281e8ffb708a161e38f
Ashraf Akl <ashrafakl@yahoo.com>
2018-01-31 19:43:42 +0200

    Enhance queries , "Commands"

--- commands/components/services/AmcPrayerService.php
+++ commands/components/services/AmcPrayerService.php
@@ -11,49 +11,44 @@
  * @version 1.0
  */
 class AmcPrayerService extends AmcService {
-    
+
     public function setInformation() {
         $this->information['table'] = 'prayer_times';
     }
-    
-    public function getData(){
+
+    public function getData() {
         return array();
     }
-    
-    protected function update(){        
+
+    protected function update() {
         $currentDate = date("Y-m-d") . " ";
         Yii::app()->db->createCommand('delete from prayer_times')->execute();
         $queryInsert = "INSERT INTO prayer_times (city_id, fajr, sunrise, dhuhr, asr, maghrib, isha) VALUES ";
-        $queryData   = array();
-        $cities = Yii::app()->db->createCommand('select * from services_cities')->queryAll();
-        foreach ($cities AS $city){
-            $city_name = urlencode($city['city']);
-            $url = "http://www.islamicfinder.org/prayer_service.php?city={$city_name}&latitude={$city['latitude']}&longitude={$city['longitude']}&timezone={$city['timezone']}&HanfiShafi=1&pmethod=2&simpleFormat=xml";
-            $xmlData = $this->getXmlFromUrl($url);
-            if ($xmlData) {
-//                $doc = new DOMDocument();
-//                @$doc->loadXML($xmlData);
-//                $items = $doc->getElementsByTagName("item");
-//                $itemCount = $items->length;
-                $data = new SimpleXMLElement($xmlData);
-                $fajr = strtotime($currentDate . (string) $data->fajr . ":00");
-                $sunrise = strtotime($currentDate . (string) $data->sunrise);
-                $dhuhrString = (string) $data->dhuhr;
-                $dhuhrTime = explode(":", $dhuhrString);
-                if ($dhuhrTime[0] > 1) {
-                    $dhuhr = strtotime($currentDate . $dhuhrString);
-                } else {
-                    $dhuhr = strtotime($currentDate . $dhuhrString) + (12 * 60 * 60);
+        $queryData = array();
+        $cities = Yii::app()->db->createCommand('select c.code, c.prayer_method as method, cc.city_id , cc.timezone, cc.latitude, cc.longitude
+        from countries c 
+        join services_cities cc on c.code = cc.country_code')->queryAll();
+        $key = Yii::app()->params['muslimsalat'] = "36f27a1d7d7b914a03514660096307f4";
+        $time = time();
+//          $url = "http://www.islamicfinder.org/prayer_service.php?city={$city_name}&latitude={$city['latitude']}&longitude={$city['longitude']}&timezone={$city['timezone']}&HanfiShafi=1&pmethod=2&simpleFormat=xml";
+//            $url = "http://muslimsalat.com/location/date.json?key=36f27a1d7d7b914a03514660096307f4&location={$city['latitude']}&longitude={$city['longitude']}";
+//            $xmlData = $this->getXmlFromUrl($url);
+        foreach ($cities AS $city) {
+            if ($city['method']) {
+                $prayTime = new PrayTime($city['method']);
+                $times = $prayTime->getPrayerTimes($time, $city['latitude'], $city['longitude'], $city['timezone']);
+                if ($times) {
+                    $fajr = strtotime("{$currentDate} {$times[0]}");
+                    $sunrise = strtotime("{$currentDate} {$times[1]}");
+                    $dhuhr = strtotime("{$currentDate} {$times[2]}");
+                    $asr = strtotime("{$currentDate} {$times[3]}");
+                    $maghrib = strtotime("{$currentDate} {$times[5]}");
+                    $isha = strtotime("{$currentDate} {$times[6]}");
+                    $queryData[] = sprintf(" (%d, %d, %d, %d, %d, %d, %d) ", $city['city_id'], $fajr, $sunrise, $dhuhr, $asr, $maghrib, $isha);
                 }
-                $asr = strtotime($currentDate . (string) $data->asr) + (12 * 60 * 60);
-                $maghrib = strtotime($currentDate . (string) $data->maghrib) + (12 * 60 * 60);
-                $isha = strtotime($currentDate . (string) $data->isha) + (12 * 60 * 60);                
-
-                $queryData[] = sprintf(" (%d, %d, %d, %d, %d, %d, %d) ", $city['city_id'], $fajr, $sunrise, $dhuhr, $asr, $maghrib, $isha);
             }
         }
-        
-        if(count($queryData)){
+        if (count($queryData)) {
             $query = $queryInsert . implode(", ", $queryData);
             Yii::app()->db->createCommand($query)->execute();
         }
